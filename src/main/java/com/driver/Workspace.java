699babeddb89a731554b6bbccb2ddd00386ae182
Vishal Singh <vishalsingh23101999@gmail.com>
2024-04-23 11:04:32 +0530

    change in workspace

--- src/main/java/com/driver/Workspace.java
+++ src/main/java/com/driver/Workspace.java
@@ -1,41 +1,53 @@
 package com.driver;
 
+import org.apache.commons.lang3.tuple.Pair;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
 
-public class Workspace extends Gmail {
+public class Workspace extends Gmail{
 
-    private List<Meeting> calendar; // Stores all the meetings
+    private ArrayList<Meeting> calendar;
 
     public Workspace(String emailId) {
-        super(emailId);
-        calendar = new ArrayList<>();
+        // The inboxCapacity is equal to the maximum value an integer can store.
+        super(emailId, Integer.MAX_VALUE);
+        this.calendar = new ArrayList<>();
     }
 
-    public void addMeeting(Meeting meeting) {
-        // Add the meeting to calendar
+    public void addMeeting(Meeting meeting){
+        //add the meeting to calendar
         calendar.add(meeting);
     }
 
-    public int findMaxMeetings() {
-        // Sort meetings by their end times
-        Collections.sort(calendar, Comparator.comparing(Meeting::getEndTime));
+    public int findMaxMeetings(){
+        // find the maximum number of meetings you can attend
+        // at a particular time, you can only be present in at most one meeting
+        // if you want to attend a meeting, you must join it at its start time and leave at end time.
 
-        int maxMeetings = 0;
-        LocalTime lastEndTime = LocalTime.MIN;
+        ArrayList<Pair<LocalTime, Integer>> endTimes = new ArrayList<>();
 
-        for (Meeting meeting : calendar) {
-            // If the meeting starts after the end time of the last meeting attended, attend it
-            if (meeting.getStartTime().isAfter(lastEndTime)) {
-                maxMeetings++;
-                lastEndTime = meeting.getEndTime();
+        for (int i = 0; i < calendar.size(); i++) {
+            endTimes.add(Pair.of(calendar.get(i).getEndTime(), i));
+        }
+
+        Collections.sort(endTimes);
+
+        LocalTime time_limit = endTimes.get(0).getLeft();
+
+        int cnt = 0;
+        if(!endTimes.isEmpty()) {
+            cnt += 1;
+        }
+
+        for (int i = 1; i < endTimes.size(); i++) {
+            if (calendar.get(endTimes.get(i).getRight()).getStartTime().compareTo(time_limit) > 0) {
+
+                cnt += 1;
+                time_limit = endTimes.get(i).getLeft();
             }
         }
 
-        return maxMeetings;
+        return cnt;
     }
-
 }
